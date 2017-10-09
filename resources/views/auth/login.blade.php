ce35d3d671977a2859e5e3d0c8cea6afb9527b52
Diego <brad.duchen@gmail.com>
2017-10-08 21:21:19 -0400

    agregar vistas auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,15 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('idcredencial') ? ' has-error' : '' }}">
+                            <label for="idcredencial" class="col-md-4 control-label">CuentaCredencial</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="idcredencial" type="text" class="form-control" name="idcredencial" value="{{ old('idcredencial') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('idcredencial'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('idcredencial') }}</strong>
                                     </span>
                                 @endif
                             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,15 +25,15 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('idcredencial') ? ' has-error' : '' }}">
+                            <label for="idcredencial" class="col-md-4 control-label">Cuenta Credencial</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="idcredencial" type="text" class="form-control" name="idcredencial" value="{{ old('idcredencial') }}" required>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('idcredencial'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('idcredencial') }}</strong>
                                     </span>
                                 @endif
                             </div>
